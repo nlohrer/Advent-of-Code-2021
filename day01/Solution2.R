279438f1f129b2a92ef5f6a8f94482a8343b9094
apsa <n.Lohrer@campus.lmu.de>
2021-12-01 18:27:34 +0100

    Add alternative (loopless) solution for day 1 part 2

--- day01/Solution2.R
+++ day01/Solution2.R
@@ -7,3 +7,11 @@ for (i in 1:length(measurementWindows)) {
 
 amountOfIncreases <- sum(ifelse(diff(measurementWindows) > 0, 1, 0))
 print(amountOfIncreases)
+
+#alternative
+data <- scan("input", sep = "\n")
+
+measurementWindowsAlt <- data[c(-length(data), -(length(data) - 1))] + data[c(-1, -length(data))] + data[c(-1, -2)]
+
+amountOfIncreasesAlt <- sum(ifelse(diff(measurementWindowsAlt) > 0, 1, 0))
+print(amountOfIncreasesAlt)
